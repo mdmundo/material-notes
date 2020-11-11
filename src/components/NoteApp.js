511b84a4b8f0c1f206fa25b06cca62e0416cb956
Edmundo Paulino <manyymoore@gmail.com>
2020-11-11 16:57:52 -0300

    Copyright

--- src/components/NoteApp.js
+++ src/components/NoteApp.js
@@ -4,11 +4,25 @@ import Avatar from '@material-ui/core/Avatar';
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import EventNoteIcon from '@material-ui/icons/EventNote';
+import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
-import notesReducer from '../reducers/notes';
-import NotesContext from '../context/notes-context';
-import NoteList from './NoteList';
 import AddNoteForm from './AddNoteForm';
+import NoteList from './NoteList';
+import NotesContext from '../context/notes-context';
+import notesReducer from '../reducers/notes';
+
+const Copyright = () => {
+  return (
+    <Typography variant='body2' color='textSecondary' align='center'>
+      {'Created by '}
+      <Link color='inherit' href='https://github.com/mdmundo'>
+        Edmundo Paulino
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+};
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,6 +40,14 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center'
+  },
+  footer: {
+    padding: theme.spacing(3, 2),
+    marginTop: 'auto',
+    backgroundColor:
+      theme.palette.type === 'light'
+        ? theme.palette.grey[200]
+        : theme.palette.grey[800]
   }
 }));
 
@@ -61,6 +83,11 @@ const NoteApp = () => {
         </div>
         <NoteList />
       </Container>
+      <footer className={classes.footer}>
+        <Container maxWidth='sm'>
+          <Copyright />
+        </Container>
+      </footer>
     </NotesContext.Provider>
   );
 };
