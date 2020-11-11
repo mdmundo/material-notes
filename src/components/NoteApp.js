b87f0bf15e29ab3aef1b98c4585890e0bba76b96
Edmundo Paulino <manyymoore@gmail.com>
2020-11-11 13:50:16 -0300

    Styling

--- src/components/NoteApp.js
+++ src/components/NoteApp.js
@@ -29,22 +29,67 @@
 // };
 
 import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Divider from '@material-ui/core/Divider';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh'
+  },
+  main: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(2)
+  },
+  list: {
     width: '100%',
     maxWidth: '36ch',
     backgroundColor: theme.palette.background.paper
   },
   inline: {
     display: 'inline'
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column'
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  upper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  },
+  grid: {
+    paddingTop: theme.spacing(3)
   }
 }));
 
@@ -52,79 +97,100 @@ const AlignItemsList = () => {
   const classes = useStyles();
 
   return (
-    <List className={classes.root}>
-      <ListItem alignItems='flex-start'>
-        <ListItemAvatar>
-          <Avatar
-            alt='Remy Sharp'
-            src='https://randomuser.me/api/portraits/women/10.jpg'
-          />
-        </ListItemAvatar>
-        <ListItemText
-          primary='Brunch this weekend?'
-          secondary={
-            <>
-              <Typography
-                component='span'
-                variant='body2'
-                className={classes.inline}
-                color='textPrimary'>
-                Ali Connors
-              </Typography>
-              {" — I'll be in your neighborhood doing errands this…"}
-            </>
-          }
-        />
-      </ListItem>
-      <Divider variant='inset' component='li' />
-      <ListItem alignItems='flex-start'>
-        <ListItemAvatar>
-          <Avatar
-            alt='Travis Howard'
-            src='https://randomuser.me/api/portraits/women/82.jpg'
-          />
-        </ListItemAvatar>
-        <ListItemText
-          primary='Summer BBQ'
-          secondary={
-            <>
-              <Typography
-                component='span'
-                variant='body2'
-                className={classes.inline}
-                color='textPrimary'>
-                to Scott, Alex, Jennifer
-              </Typography>
-              {" — Wish I could come, but I'm out of town this…"}
-            </>
-          }
-        />
-      </ListItem>
-      <Divider variant='inset' component='li' />
-      <ListItem alignItems='flex-start'>
-        <ListItemAvatar>
-          <Avatar
-            alt='Cindy Baker'
-            src='https://randomuser.me/api/portraits/women/50.jpg'
-          />
-        </ListItemAvatar>
-        <ListItemText
-          primary='Oui Oui'
-          secondary={
-            <>
-              <Typography
-                component='span'
-                variant='body2'
-                className={classes.inline}
-                color='textPrimary'>
-                Sandra Adams
-              </Typography>
-              {' — Do you have Paris recommendations? Have you ever…'}
-            </>
-          }
-        />
-      </ListItem>
-    </List>
+    <div className={classes.root}>
+      <Container component='main' className={classes.main} maxWidth='xs'>
+        <CssBaseline />
+        <div className={classes.upper}>
+          <Avatar className={classes.avatar}>
+            <EventNoteIcon />
+          </Avatar>
+          <Typography component='h1' variant='h3'>
+            Notes
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant='outlined'
+              margin='normal'
+              fullWidth
+              id='title'
+              label='Title'
+              name='title'
+              autoComplete='off'
+              autoFocus
+            />
+            <TextField
+              variant='outlined'
+              margin='normal'
+              fullWidth
+              name='body'
+              label='Body'
+              type='body'
+              id='body'
+              autoComplete='off'
+              multiline
+            />
+            <Button
+              type='submit'
+              fullWidth
+              variant='contained'
+              color='primary'
+              className={classes.submit}>
+              Add Note
+            </Button>
+          </form>
+        </div>
+        <Grid item className={classes.grid}>
+          <Paper className={classes.paper}>
+            <List className={classes.list}>
+              <ListItem>
+                <ListItemIcon>
+                  <Checkbox />
+                </ListItemIcon>
+                <ListItemText
+                  primary='Note title'
+                  secondary='Note body, body, body, body, body, body, body, body, body, body, body, body, body'
+                />
+                <ListItemSecondaryAction>
+                  <IconButton edge='end' aria-label='delete'>
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+              <Divider variant='inset' component='li' />
+              <ListItem>
+                <ListItemIcon>
+                  <Checkbox />
+                </ListItemIcon>
+                <ListItemText
+                  primary='Note title'
+                  secondary='Note body, body, body, body, body, body, body, body, body, body, body, body, body'
+                />
+                <ListItemSecondaryAction>
+                  <IconButton edge='end' aria-label='delete'>
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+              <Divider variant='inset' component='li' />
+              <ListItem>
+                <ListItemIcon>
+                  <Checkbox />
+                </ListItemIcon>
+                <ListItemText
+                  primary='Note title'
+                  secondary='Note body, body, body, body, body, body, body, body, body, body, body, body, body'
+                />
+                <ListItemSecondaryAction>
+                  <IconButton edge='end' aria-label='delete'>
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            </List>
+          </Paper>
+        </Grid>
+      </Container>
+    </div>
   );
 };
 
