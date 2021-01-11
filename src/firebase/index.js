c0080b38a154aa476c2a6d574dde493e86c6b25c
Edmundo Paulino <manyymoore@gmail.com>
2021-01-11 15:32:03 -0300

    Updates

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,13 +1,15 @@
 import firebase from 'firebase';
 
-firebase.initializeApp({
+const firebaseConfig = {
   apiKey: 'AIzaSyDr8Zix9phP7yKXnd47btENUXyXQvA3Eec',
   authDomain: 'mdmundonotes.firebaseapp.com',
   projectId: 'mdmundonotes',
   storageBucket: 'mdmundonotes.appspot.com',
   messagingSenderId: '1019982159986',
-  appId: '1:1019982159986:web:2da20e8a80ec22ccfaa2eb'
-});
+  appId: '1:1019982159986:web:c7098cc516c4fa26faa2eb'
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
