e0dfc006e340f57a03f8af71884104862ee35c9f
Edmundo Paulino <manyymoore@gmail.com>
2021-01-19 20:04:23 -0300

    Updates

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,20 @@
 import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 import { firebase, provider } from '../firebase';
 
+const useStyles = makeStyles((theme) => ({
+  login: {
+    marginTop: theme.spacing(7)
+  }
+}));
+
 const Login = () => {
+  const classes = useStyles();
+
   return (
     <Button
+      className={classes.login}
+      fullWidth
       variant='contained'
       color='primary'
       onClick={() => {
