64ee49603140c2bdbce99a13dbffca1c30fc8538
Edmundo Paulino <manyymoore@gmail.com>
2021-01-19 14:39:15 -0300

    Updates

--- src/actions/auth.js
+++
@@ -1,9 +0,0 @@
-import { firebase, provider } from '../firebase';
-
-export const startLogin = () => {
-  firebase.auth().signInWithPopup(provider);
-};
-
-export const startLogout = () => {
-  firebase.auth().signOut();
-};
